c468f1ae5798cc5dbc64efe73f57b3872ba7cb18
Susi Lehtola <susi.lehtola@gmail.com>
2018-10-27 19:17:33 +0300

    Add license boilerplates.

--- src/atomic/dftgrid.cpp
+++ src/atomic/dftgrid.cpp
@@ -1,6 +1,21 @@
 /*
  *                This source code is part of
  *
+ *                          HelFEM
+ *                             -
+ * Finite element methods for electronic structure calculations on small systems
+ *
+ * Written by Susi Lehtola, 2018-
+ * Copyright (c) 2018- Susi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+/*
+ *                This source code is part of
+ *
  *                     E  R  K  A  L  E
  *                             -
  *                       DFT from Hel
