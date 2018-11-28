7a10104c5d8337790318277c3f6b7a578db17011
Susi Lehtola <susi.lehtola@gmail.com>
2018-11-28 11:23:42 +0200

    Drop duplicate license boilerplate

--- src/diatomic/dftgrid.h
+++ src/diatomic/dftgrid.h
@@ -13,22 +13,6 @@
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  */
-/*
- *                This source code is part of
- *
- *                     E  R  K  A  L  E
- *                             -
- *                       DFT from Hel
- *
- * Written by Susi Lehtola, 2011
- * Copyright (c) 2011, Susi Lehtola
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- */
-
 
 #ifndef DFTGRID
 #define DFTGRID
