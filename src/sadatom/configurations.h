f730ee77e6ab443c21764ac3cd23accb15d11df2
Susi Lehtola <susi.lehtola@gmail.com>
2018-11-28 11:45:25 +0200

    Add cationic configurations too.

--- src/sadatom/configurations.h
+++ src/sadatom/configurations.h
@@ -9,4 +9,7 @@ typedef std::pair<int, int> occ_t;
 /// Get configuration (l, nelectrons) for atom with charge Z
 std::vector<occ_t> get_configuration(int Z);
 
+/// Get configuration (l, nelectrons) for cation of atom Z
+std::vector<occ_t> get_cationic_configuration(int Z);
+
 #endif
