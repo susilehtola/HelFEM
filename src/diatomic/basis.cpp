susilehtola/HelFEM

commit f6db22deb0efb52ceef9d8d5cd6f4390a955a15b

Restructuring some of the radial basis code

src/diatomic/basis.cpp
@@ -38,27 +38,6 @@ namespace helfem {
       RadialBasis::RadialBasis() {
       }
 
-      RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, int num_el, double rmax, int igrid, double zexp) {
-        // Polynomial basis
-        poly=poly_->copy();
-
-        // Get quadrature rule
-        chebyshev::chebyshev(n_quad,xq,wq);
-        for(size_t i=0;i<xq.n_elem;i++) {
-          if(!std::isfinite(xq[i]))
-            printf("xq[%i]=%e\n",(int) i, xq[i]);
-          if(!std::isfinite(wq[i]))
-            printf("wq[%i]=%e\n",(int) i, wq[i]);
-        }
-
-        // Evaluate polynomials at quadrature points
-        poly->eval(xq,bf,df);
-
-        // Get boundary values
-        bval=utils::get_grid(rmax,num_el,igrid,zexp);
-        //bval.print("Element boundaries");
-      }
-
       RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, const arma::vec & bval_) {
 	// Polynomial basis
         poly=poly_->copy();
@@ -79,18 +58,17 @@ namespace helfem {
         bval=bval_;
       }
 
-      RadialBasis::RadialBasis(const RadialBasis & old) {
-        *this=old;
+      RadialBasis::RadialBasis(const RadialBasis & rh) {
+        *this = rh;
       }
 
-      RadialBasis & RadialBasis::operator=(const RadialBasis & old) {
-        xq=old.xq;
-        wq=old.wq;
-        poly=old.poly->copy();
-        bf=old.bf;
-        df=old.df;
-        bval=old.bval;
-
+      RadialBasis & RadialBasis::operator=(const RadialBasis & rh) {
+        xq=rh.xq;
+        wq=rh.wq;
+        poly=rh.poly->copy();
+        bf=rh.bf;
+        df=rh.df;
+        bval=rh.bval;
         return *this;
       }
 
@@ -129,7 +107,7 @@ namespace helfem {
       }
 
       polynomial_basis::PolynomialBasis * RadialBasis::get_basis(const polynomial_basis::PolynomialBasis * polynom, size_t iel) const {
-        polynomial_basis::PolynomialBasis *p(polynom->copy());
+        polynomial_basis::PolynomialBasis * p(polynom->copy());
         if(iel==bval.n_elem-2) {
           // Boundary condition at infinity
           p->drop_last();
@@ -340,7 +318,7 @@ namespace helfem {
         double mumax(bval(iel+1));
 
         // Integral by quadrature
-        polynomial_basis::PolynomialBasis *p(get_basis(poly,iel));
+        polynomial_basis::PolynomialBasis * p(get_basis(poly,iel));
         arma::mat tei(quadrature::twoe_integral(mumin,mumax,alpha,beta,xq,wq,p,L,M,legtab));
         delete p;
 
@@ -430,24 +408,7 @@ namespace helfem {
         return rmid*arma::ones<arma::vec>(xq.n_elem)+rlen*xq;
       }
 
-      TwoDBasis::TwoDBasis() {
-      }
-
-      TwoDBasis::TwoDBasis(int Z1_, int Z2_, double Rbond, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, const arma::ivec & lmax, int igrid, double zexp, int lpad, bool legendre) {
-        // Nuclear charge
-        Z1=Z1_;
-        Z2=Z2_;
-        Rhalf=Rbond/2.0;
-
-        // Compute max mu value
-        double mumax=utils::arcosh(rmax/Rhalf);
-
-        if(legendre) printf("rmax = %e yields mumax = %e\n",rmax,mumax);
-
-        // Construct radial basis
-        radial=RadialBasis(poly, n_quad, num_el, mumax, igrid, zexp);
-
-        // Store values
+      void lm_to_l_m(const arma::ivec & lmax, arma::ivec & lval, arma::ivec & mval) {
         {
           std::vector<arma::sword> lv, mv;
           for(size_t mabs=0;mabs<lmax.n_elem;mabs++)
@@ -462,16 +423,25 @@ namespace helfem {
           lval=arma::conv_to<arma::ivec>::from(lv);
           mval=arma::conv_to<arma::ivec>::from(mv);
         }
+      }
 
-        if(legendre) {
-          arma::imat bang(lval.n_elem,2);
-          bang.col(0)=lval;
-          bang.col(1)=mval;
-          bang.print("Angular basis");
-        }
+      TwoDBasis::TwoDBasis() {
+      }
+
+      TwoDBasis::TwoDBasis(int Z1_, int Z2_, double Rhalf_, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_, int lpad, bool legendre) {
+        // Nuclear charge
+        Z1=Z1_;
+        Z2=Z2_;
+        Rhalf=Rhalf_;
+
+        // Construct radial basis
+        radial=RadialBasis(poly, n_quad, bval);
+        // Angular basis
+        lval=lval_;
+        mval=mval_;
 
         // Gaunt coefficients
-        int gmax(arma::max(lmax)+2);
+        int gmax(arma::max(lval)+2);
 
         // Legendre function values
         if(legendre) {
@@ -558,19 +528,6 @@ namespace helfem {
 
           gaunt=gaunt::Gaunt(lrval,Mmax,midval,Mmax,lrval,Mmax);
         }
-      }
-
-      TwoDBasis::TwoDBasis(int Z1_, int Z2_, double Rhalf_, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_) {
-        // Nuclear charge
-        Z1=Z1_;
-        Z2=Z2_;
-        Rhalf=Rhalf_;
-
-        // Construct radial basis
-        radial=RadialBasis(poly, n_quad, bval);
-        // Angular basis
-        lval=lval_;
-        mval=mval_;
       }
 
       TwoDBasis::~TwoDBasis() {

src/diatomic/basis.h
@@ -52,14 +52,13 @@ namespace helfem {
         /// Dummy constructor
         RadialBasis();
         /// Construct radial basis
-        RadialBasis(const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int igrid, double zexp);
-        /// Construct radial basis
         RadialBasis(const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval);
-        // Copy constructor
-        RadialBasis(const RadialBasis & old);
-        // Assignment
-        RadialBasis & operator=(const RadialBasis & old);
-        /// Destructor
+
+        /// Explicit copy constructor because of shared pointer
+        RadialBasis(const RadialBasis & rh);
+        /// Explicit assignment operator because of shared pointer
+        RadialBasis & operator=(const RadialBasis & rh);
+        /// Explicit destructor because of shared pointer
         ~RadialBasis();
 
         /// Get number of quadrature points
@@ -132,6 +131,9 @@ namespace helfem {
       /// Equivalence operator
       bool operator==(const lmidx_t & lh, const lmidx_t & rh);
 
+      /// l(m) array to l, m arrays
+      void lm_to_l_m(const arma::ivec & lmmax, arma::ivec & lval, arma::ivec & mval);
+
       /// Two-dimensional basis set
       class TwoDBasis {
         /// Nuclear charges
@@ -180,9 +182,7 @@ namespace helfem {
         // Dummy constructor
         TwoDBasis();
         /// Constructor
-        TwoDBasis(int Z1, int Z2, double Rbond, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, const arma::ivec & lmax, int igrid, double zexp, int lpad, bool legendre=true);
-        /// Constructor
-        TwoDBasis(int Z1, int Z2, double Rhalf, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval, const arma::ivec & mval);
+        TwoDBasis(int Z1, int Z2, double Rhalf, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval, const arma::ivec & mval, int lpad=0, bool legendre=true);
         /// Destructor
         ~TwoDBasis();
 

src/diatomic/corebasis.cpp
@@ -19,6 +19,8 @@
 #include "../general/elements.h"
 #include "../general/scf_helpers.h"
 #include "../general/model_potential.h"
+#include "../general/utils.h"
+#include "../atomic/basis.h"
 #include "basis.h"
 #include "twodquadrature.h"
 #include <cfloat>
@@ -29,7 +31,15 @@ void eval(int Z1, int Z2, double Rrms1, double Rrms2, double Rbond, const polyno
 
   int lpad=0;
   int symm=1;
-  diatomic::basis::TwoDBasis basis(Z1, Z2, Rbond, poly, Nquad, Nelem, Rmax, lmmax, igrid, zexp, lpad, false);
+
+  double Rhalf=0.5*Rbond;
+  double mumax(utils::arcosh(Rmax/Rhalf));
+  arma::vec bval(atomic::basis::normal_grid(Nelem, mumax, igrid, zexp));
+
+  arma::ivec lval, mval;
+  diatomic::basis::lm_to_l_m(lmmax,lval,mval);
+
+  diatomic::basis::TwoDBasis basis(Z1, Z2, Rhalf, poly, Nquad, bval, lval, mval, lpad, false);
 
   bool diag=true;
   // Symmetry indices

src/atomic/basis.cpp
@@ -34,8 +34,8 @@ namespace helfem {
       RadialBasis::RadialBasis() {
       }
 
-      RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, int num_el, double rmax, int igrid, double zexp) {
-        // Polynomial basis
+      RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, const arma::vec & bval_) {
+	// Polynomial basis
         poly=poly_->copy();
 
         // Get quadrature rule
@@ -50,10 +50,26 @@ namespace helfem {
         // Evaluate polynomials at quadrature points
         poly->eval(xq,bf,df);
 
-        // Get boundary values
-        bval=utils::get_grid(rmax,num_el,igrid,zexp);
+        // Element boundaries
+        bval=bval_;
+      }
 
-        //bval.print("Element boundaries");
+      RadialBasis::RadialBasis(const RadialBasis & rh) {
+        *this = rh;
+      }
+
+      RadialBasis & RadialBasis::operator=(const RadialBasis & rh) {
+        xq=rh.xq;
+        wq=rh.wq;
+        poly=rh.poly->copy();
+        bf=rh.bf;
+        df=rh.df;
+        bval=rh.bval;
+        return *this;
+      }
+
+      RadialBasis::~RadialBasis() {
+        delete poly;
       }
 
       static arma::vec concatenate_grid(const arma::vec & left, const arma::vec & right) {
@@ -74,22 +90,25 @@ namespace helfem {
         return ret;
       }
 
-      RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, int num_el0, int Zm, int Zlr, double Rhalf, int num_el, double rmax, int igrid, double zexp) {
-	// Polynomial basis
-        poly=poly_->copy();
+      arma::vec normal_grid(int num_el, double rmax, int igrid, double zexp) {
+        return utils::get_grid(rmax,num_el,igrid,zexp);
+      }
 
-        // Get quadrature rule
-        chebyshev::chebyshev(n_quad,xq,wq);
-        for(size_t i=0;i<xq.n_elem;i++) {
-          if(!std::isfinite(xq[i]))
-            printf("xq[%i]=%e\n",(int) i, xq[i]);
-          if(!std::isfinite(wq[i]))
-            printf("wq[%i]=%e\n",(int) i, wq[i]);
+      arma::vec finite_nuclear_grid(int num_el, double rmax, int igrid, double zexp, int num_el_nuc, double rnuc, int igrid_nuc, double zexp_nuc) {
+        if(num_el_nuc) {
+          // Grid for the finite nucleus
+          arma::vec bnuc(utils::get_grid(rnuc,num_el_nuc,igrid_nuc,zexp_nuc));
+          // and the one for the electrons
+          arma::vec belec(utils::get_grid(rmax-rnuc,num_el,igrid,zexp));
+
+          arma::vec bnucel(concatenate_grid(bnuc,bnuc));
+          return concatenate_grid(bnucel,belec);
+        } else {
+          return utils::get_grid(rmax,num_el,igrid,zexp);
         }
+      }
 
-        // Evaluate polynomials at quadrature points
-        poly->eval(xq,bf,df);
-
+      arma::vec offcenter_nuclear_grid(int num_el0, int Zm, int Zlr, double Rhalf, int num_el, double rmax, int igrid, double zexp) {
         // First boundary at
         int b0used = (Zm != 0);
         double b0=Zm*Rhalf/(Zm+Zlr);
@@ -121,6 +140,7 @@ namespace helfem {
         }
         arma::vec bval2=utils::get_grid(b2-b1,num_el,igrid,zexp);
 
+        arma::vec bval;
         if(b0used && b1used) {
           bval=concatenate_grid(bval0,bval1);
         } else if(b0used) {
@@ -133,46 +153,68 @@ namespace helfem {
         } else {
           bval=bval2;
         }
-        bval.print("Element boundaries");
+
+        return bval;
       }
 
-      RadialBasis::RadialBasis(const polynomial_basis::PolynomialBasis * poly_, int n_quad, const arma::vec & bval_) {
-	// Polynomial basis
-        poly=poly_->copy();
+      arma::vec form_grid(modelpotential::nuclear_model_t model, double Rrms, int Nelem, double Rmax, int igrid, double zexp, int Nelem0, int igrid0, double zexp0, int Z, int Zl, int Zr, double Rhalf) {
+        // Construct the radial basis
+        arma::vec bval;
+        if(model != modelpotential::POINT_NUCLEUS) {
+          printf("Finite-nucleus grid\n");
 
-        // Get quadrature rule
-        chebyshev::chebyshev(n_quad,xq,wq);
-        for(size_t i=0;i<xq.n_elem;i++) {
-          if(!std::isfinite(xq[i]))
-            printf("xq[%i]=%e\n",(int) i, xq[i]);
-          if(!std::isfinite(wq[i]))
-            printf("wq[%i]=%e\n",(int) i, wq[i]);
+          if(Zl != 0 || Zr != 0)
+            throw std::logic_error("Off-center nuclei not supported in finite nucleus mode!\n");
+
+          double rnuc;
+          if(model == modelpotential::HOLLOW_NUCLEUS)
+            rnuc = Rrms;
+          else if(model == modelpotential::SPHERICAL_NUCLEUS)
+            rnuc = sqrt(5.0/3.0)*Rrms;
+          else if(model == modelpotential::GAUSSIAN_NUCLEUS)
+            rnuc = 3*Rrms;
+          else
+            throw std::logic_error("Nuclear grid not handled!\n");
+
+          bval=atomic::basis::finite_nuclear_grid(Nelem,Rmax,igrid,zexp,Nelem0,rnuc,igrid0,zexp0);
+
+        } else if(Zl != 0 || Zr != 0) {
+          printf("Off-center grid\n");
+          bval=atomic::basis::offcenter_nuclear_grid(Nelem0,Z,std::max(Zl,Zr),Rhalf,Nelem,Rmax,igrid,zexp);
+        } else {
+          printf("Normal grid\n");
+          bval=atomic::basis::normal_grid(Nelem,Rmax,igrid,zexp);
         }
 
-        // Evaluate polynomials at quadrature points
-        poly->eval(xq,bf,df);
+        bval.print("Grid");
 
-        // Element boundaries
-        bval=bval_;
+        return bval;
       }
 
-      RadialBasis::RadialBasis(const RadialBasis & old) {
-        *this=old;
-      }
+      void angular_basis(int lmax, int mmax, arma::ivec & lval, arma::ivec & mval) {
+        size_t nang=0;
+        for(int l=0;l<=lmax;l++)
+          nang+=2*std::min(mmax,l)+1;
 
-      RadialBasis & RadialBasis::operator=(const RadialBasis & old) {
-        xq=old.xq;
-        wq=old.wq;
-        poly=old.poly->copy();
-        bf=old.bf;
-        df=old.df;
-        bval=old.bval;
+        // Allocate memory
+        lval.zeros(nang);
+        mval.zeros(nang);
 
-        return *this;
-      }
-
-      RadialBasis::~RadialBasis() {
-        delete poly;
+        // Store values
+        size_t iang=0;
+        for(int mabs=0;mabs<=mmax;mabs++)
+          for(int l=mabs;l<=lmax;l++) {
+            lval(iang)=l;
+            mval(iang)=mabs;
+            iang++;
+            if(mabs>0) {
+              lval(iang)=l;
+              mval(iang)=-mabs;
+              iang++;
+            }
+          }
+        if(iang!=lval.n_elem)
+          throw std::logic_error("Error.\n");
       }
 
       void RadialBasis::add_boundary(double r) {
@@ -227,7 +269,7 @@ namespace helfem {
       }
 
       polynomial_basis::PolynomialBasis * RadialBasis::get_basis(const polynomial_basis::PolynomialBasis * polynom, size_t iel) const {
-        polynomial_basis::PolynomialBasis *p(polynom->copy());
+        polynomial_basis::PolynomialBasis * p(polynom->copy());
 	if(iel==0)
           p->drop_first();
 	if(iel==bval.n_elem-2)
@@ -713,94 +755,17 @@ namespace helfem {
       TwoDBasis::TwoDBasis() {
       }
 
-      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model_, double Rrms_, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int lmax, int mmax, int igrid, double zexp) {
+      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model_, double Rrms_, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_, int Zl_, int Zr_, double Rhalf_) {
         // Nuclear charge
         Z=Z_;
-        Zl=0;
-        Zr=0;
-        Rhalf=0.0;
+        Zl=Zl_;
+        Zr=Zr_;
+        Rhalf=Rhalf_;
         model=model_;
         Rrms=Rrms_;
 
         // Construct radial basis
-        radial=RadialBasis(poly, n_quad, num_el, rmax, igrid, zexp);
-        // Add extra node at nuclear radius if necessary
-        if(model == modelpotential::HOLLOW_NUCLEUS) {
-          radial.add_boundary(Rrms);
-        } else if(model == modelpotential::SPHERICAL_NUCLEUS) {
-          radial.add_boundary(sqrt(5.0/3.0)*Rrms);
-        }
-
-        // Construct angular basis
-        angular_basis(lmax,mmax,lval,mval);
-      }
-
-      void angular_basis(int lmax, int mmax, arma::ivec & lval, arma::ivec & mval) {
-        size_t nang=0;
-        for(int l=0;l<=lmax;l++)
-          nang+=2*std::min(mmax,l)+1;
-
-        // Allocate memory
-        lval.zeros(nang);
-        mval.zeros(nang);
-
-        // Store values
-        size_t iang=0;
-        for(int mabs=0;mabs<=mmax;mabs++)
-          for(int l=mabs;l<=lmax;l++) {
-            lval(iang)=l;
-            mval(iang)=mabs;
-            iang++;
-
-            if(mabs>0) {
-              lval(iang)=l;
-              mval(iang)=-mabs;
-              iang++;
-            }
-          }
-        if(iang!=lval.n_elem)
-          throw std::logic_error("Error.\n");
-      }
-
-      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el0, int num_el, double rmax, int lmax, int mmax, int igrid, double zexp, int Zl_, int Zr_, double Rhalf_) {
-        // Nuclear charge
-        Z=Z_;
-        Zl=Zl_;
-        Zr=Zr_;
-        Rhalf=Rhalf_;
-
-        // Construct radial basis
-        radial=RadialBasis(poly, n_quad, num_el0, Z_, std::max(Zl_,Zr_), Rhalf, num_el, rmax, igrid, zexp);
-        // Add extra node at nuclear radius if necessary
-        if((Zl != 0 || Zr != 0) && model != modelpotential::POINT_NUCLEUS)
-          throw std::logic_error("Finite off-center nuclei not implemented!\n");
-        if(model == modelpotential::HOLLOW_NUCLEUS) {
-          radial.add_boundary(Rrms);
-        } else if(model == modelpotential::SPHERICAL_NUCLEUS) {
-          radial.add_boundary(sqrt(5.0/3.0)*Rrms);
-        }
-
-        // Construct angular basis
-        angular_basis(lmax,mmax,lval,mval);
-      }
-
-      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_, int Zl_, int Zr_, double Rhalf_) {
-        // Nuclear charge
-        Z=Z_;
-        Zl=Zl_;
-        Zr=Zr_;
-        Rhalf=Rhalf_;
-
-        // Construct radial basis
         radial=RadialBasis(poly, n_quad, bval);
-        // Add extra node at nuclear radius if necessary
-        if((Zl != 0 || Zr != 0) && model != modelpotential::POINT_NUCLEUS)
-          throw std::logic_error("Finite off-center nuclei not implemented!\n");
-        if(model == modelpotential::HOLLOW_NUCLEUS) {
-          radial.add_boundary(Rrms);
-        } else if(model == modelpotential::SPHERICAL_NUCLEUS) {
-          radial.add_boundary(sqrt(5.0/3.0)*Rrms);
-        }
 
         // Construct angular basis
         lval=lval_;
@@ -1189,7 +1154,7 @@ namespace helfem {
         }
       }
 
-      arma::mat TwoDBasis::model_potential(const modelpotential::ModelPotential * model) const {
+      arma::mat TwoDBasis::model_potential(const modelpotential::ModelPotential * pot) const {
         // Full nuclear attraction matrix
         arma::mat V(Ndummy(),Ndummy());
         V.zeros();
@@ -1202,7 +1167,7 @@ namespace helfem {
 	  // Where are we in the matrix?
 	  size_t ifirst, ilast;
 	  radial.get_idx(iel,ifirst,ilast);
-	  Vrad.submat(ifirst,ifirst,ilast,ilast)+=radial.model_potential(model,iel);
+	  Vrad.submat(ifirst,ifirst,ilast,ilast)+=radial.model_potential(pot,iel);
 	}
 	// Fill elements
 	for(size_t iang=0;iang<lval.n_elem;iang++)

src/atomic/basis.h
@@ -50,16 +50,13 @@ namespace helfem {
         /// Dummy constructor
         RadialBasis();
         /// Construct radial basis
-        RadialBasis(const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int igrid, double zexp);
-        /// Construct radial basis
-        RadialBasis(const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el0, int Zm, int Zlr, double Rhalf, int num_el, double rmax, int igrid, double zexp);
-        /// Construct radial basis
         RadialBasis(const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval);
-        // Copy constructor
-        RadialBasis(const RadialBasis & old);
-        // Assignment
-        RadialBasis & operator=(const RadialBasis & old);
-        /// Destructor
+
+        /// Explicit copy constructor because of shared pointer
+        RadialBasis(const RadialBasis & rh);
+        /// Explicit assignment operator because of shared pointer
+        RadialBasis & operator=(const RadialBasis & rh);
+        /// Explicit destructor because of shared pointer
         ~RadialBasis();
 
         /// Add an element boundary
@@ -154,6 +151,15 @@ namespace helfem {
         arma::rowvec nuclear_orbital(const arma::mat & C) const;
       };
 
+      /// Get the element grid for a normal calculation
+      arma::vec normal_grid(int num_el, double rmax, int igrid, double zexp);
+      /// Get the element grid for a finite nucleus
+      arma::vec finite_nuclear_grid(int num_el, double rmax, int igrid, double zexp, int num_el_nuc, double rnuc, int igrid_nuc, double zexp_nuc);
+      /// Get the element grid in the case of off-center nuclei
+      arma::vec offcenter_nuclear_grid(int num_el0, int Zm, int Zlr, double Rhalf, int num_el, double rmax, int igrid, double zexp);
+      /// Form the grid in the general case, using the above routines
+      arma::vec form_grid(modelpotential::nuclear_model_t model, double Rrms, int Nelem, double Rmax, int igrid, double zexp, int Nelem0, int igrid0, double zexp0, int Z, int Zl, int Zr, double Rhalf);
+
       /// Constructs an angular basis
       void angular_basis(int lmax, int mmax, arma::ivec & lval, arma::ivec & mval);
 
@@ -205,10 +211,6 @@ namespace helfem {
 
       public:
         TwoDBasis();
-        /// Constructor
-        TwoDBasis(int Z, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int lmax, int mmax, int igrid, double zexp);
-        /// Constructor
-        TwoDBasis(int Z, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el0, int num_el, double rmax, int lmax, int mmax, int igrid, double zexp, int Zl, int Zr, double Rhalf);
         /// Constructor
         TwoDBasis(int Z, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval, const arma::ivec & mval, int Zl, int Zr, double Rhalf);
         /// Destructor

src/sadatom/basis.cpp
@@ -38,22 +38,17 @@ namespace helfem {
       TwoDBasis::TwoDBasis() {
       }
 
-      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model_, double Rrms_, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int lmax, int igrid, double zexp) {
+      TwoDBasis::TwoDBasis(int Z_, modelpotential::nuclear_model_t model_, double Rrms_, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, int lmax) {
         // Nuclear charge
         Z=Z_;
         model=model_;
         Rrms=Rrms_;
         // Construct radial basis
-        radial=atomic::basis::RadialBasis(poly, n_quad, num_el, rmax, igrid, zexp);
-        // Add extra node at nuclear radius if necessary
-        if(model == modelpotential::HOLLOW_NUCLEUS) {
-          radial.add_boundary(Rrms);
-        } else if(model == modelpotential::SPHERICAL_NUCLEUS) {
-          radial.add_boundary(sqrt(5.0/3.0)*Rrms);
-        }
+        radial=atomic::basis::RadialBasis(poly, n_quad, bval);
         // Angular basis
         lval=arma::linspace<arma::ivec>(0,lmax,lmax+1);
       }
+
       TwoDBasis::~TwoDBasis() {
       }
 
@@ -148,7 +143,7 @@ namespace helfem {
         }
       }
 
-      arma::mat TwoDBasis::model_potential(const modelpotential::ModelPotential * model) const {
+      arma::mat TwoDBasis::model_potential(const modelpotential::ModelPotential * pot) const {
         size_t Nrad(radial.Nbf());
         arma::mat Vrad(Nrad,Nrad);
         Vrad.zeros();
@@ -157,7 +152,7 @@ namespace helfem {
           // Where are we in the matrix?
           size_t ifirst, ilast;
           radial.get_idx(iel,ifirst,ilast);
-          Vrad.submat(ifirst,ifirst,ilast,ilast)+=radial.model_potential(model,iel);
+          Vrad.submat(ifirst,ifirst,ilast,ilast)+=radial.model_potential(pot,iel);
         }
         return Vrad;
       }

src/sadatom/basis.h
@@ -56,7 +56,7 @@ namespace helfem {
       public:
         TwoDBasis();
         /// Constructor
-        TwoDBasis(int Z, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, int num_el, double rmax, int lmax, int igrid, double zexp);
+        TwoDBasis(int Z, modelpotential::nuclear_model_t model, double Rrms, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, int lmax);
         /// Destructor
         ~TwoDBasis();
 

commit 0154157534b550abe7335af7a563a46165a5c055

Commit f6db22deb0efb52ceef9d8d5cd6f4390a955a15b inadvertently changed the signature of the diatomic TwoDBasis, changing the input from the bond length to the half-bond length. This commit reverts that change

src/diatomic/basis.cpp
@@ -428,11 +428,11 @@ namespace helfem {
       TwoDBasis::TwoDBasis() {
       }
 
-      TwoDBasis::TwoDBasis(int Z1_, int Z2_, double Rhalf_, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_, int lpad, bool legendre) {
+      TwoDBasis::TwoDBasis(int Z1_, int Z2_, double Rbond, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval_, const arma::ivec & mval_, int lpad, bool legendre) {
         // Nuclear charge
         Z1=Z1_;
         Z2=Z2_;
-        Rhalf=Rhalf_;
+        Rhalf=0.5*Rbond;
 
         // Construct radial basis
         radial=RadialBasis(poly, n_quad, bval);

src/diatomic/basis.h
@@ -182,7 +182,7 @@ namespace helfem {
         // Dummy constructor
         TwoDBasis();
         /// Constructor
-        TwoDBasis(int Z1, int Z2, double Rhalf, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval, const arma::ivec & mval, int lpad=0, bool legendre=true);
+        TwoDBasis(int Z1, int Z2, double Rbond, const polynomial_basis::PolynomialBasis * poly, int n_quad, const arma::vec & bval, const arma::ivec & lval, const arma::ivec & mval, int lpad=0, bool legendre=true);
         /// Destructor
         ~TwoDBasis();
 

src/diatomic/corebasis.cpp
@@ -39,7 +39,7 @@ void eval(int Z1, int Z2, double Rrms1, double Rrms2, double Rbond, const polyno
   arma::ivec lval, mval;
   diatomic::basis::lm_to_l_m(lmmax,lval,mval);
 
-  diatomic::basis::TwoDBasis basis(Z1, Z2, Rhalf, poly, Nquad, bval, lval, mval, lpad, false);
+  diatomic::basis::TwoDBasis basis(Z1, Z2, Rbond, poly, Nquad, bval, lval, mval, lpad, false);
 
   bool diag=true;
   // Symmetry indices