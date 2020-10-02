6572581dd38b3e05d0f38b3426965bf8fc9f89f5
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-10-02 16:29:00 +1300

    Add missing copyright headers

--- libhelfem/src/RadialPotential.h
+++ libhelfem/src/RadialPotential.h
@@ -1,3 +1,18 @@
+/*
+ *                This source code is part of
+ *
+ *                          HelFEM
+ *                             -
+ * Finite element methods for electronic structure calculations on small systems
+ *
+ * Written by Susi Lehtola, 2018-
+ * Copyright (c) 2018- Susi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
 #ifndef MODELPOTENTIAL_RADIALPOTENTIAL_H
 #define MODELPOTENTIAL_RADIALPOTENTIAL_H
 
