017c7189a561cb3889b0cdcdc9b6aa477bd416e9
Susi Lehtola <susi.lehtola@gmail.com>
2023-01-30 10:07:14 +0200

    Autogeneration of HIP derivatives

--- libhelfem/src/HIPBasis.h
+++ libhelfem/src/HIPBasis.h
@@ -27,22 +27,6 @@ namespace helfem {
     protected:
       /// LIP derivatives at nodes
       arma::vec lipxi;
-      /// Evaluate polynomials at given points
-      void eval_prim_f(const arma::vec & x, arma::mat & f, double element_length) const;
-      /// Evaluate derivatives of polynomials at given points
-      void eval_prim_df(const arma::vec & x, arma::mat & df, double element_length) const;
-      /// Evaluate second derivatives of polynomials at given points
-      void eval_prim_d2f(const arma::vec & x, arma::mat & d2f, double element_length) const;
-      /// Evaluate third derivatives of polynomials at given points
-      void eval_prim_d3f(const arma::vec & x, arma::mat & d3f, double element_length) const;
-      /// Evaluate fourth derivatives of polynomials at given points
-      void eval_prim_d4f(const arma::vec & x, arma::mat & d4f, double element_length) const;
-      /// Evaluate fifth derivatives of polynomials at given points
-      void eval_prim_d5f(const arma::vec & x, arma::mat & d5f, double element_length) const;
-      /// Evaluate sixth derivatives of polynomials at given points
-      void eval_prim_d6f(const arma::vec & x, arma::mat & d6f, double element_length) const;
-      /// Evaluate seventh derivatives of polynomials at given points
-      void eval_prim_d7f(const arma::vec & x, arma::mat & d7f, double element_length) const;
     public:
       /// Constructor
       HIPBasis(const arma::vec & x0, int id=5);
