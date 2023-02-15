susilehtola/HelFEM

commit d62f1c1b6fb492303fca54a8a832ba7803b7ca1d

More refactoring of the atomic code.

libhelfem/src/utils.cpp
@@ -44,24 +44,29 @@ namespace helfem {
       return y;
     }
 
-    arma::vec bessel_il(const arma::vec & x, int L) {
-      arma::vec y(x);
-      for(size_t i=0;i<x.n_elem;i++)
-        // GSL calculates exp(-|x|)k_l(x)
-	y(i)=exp(std::abs(x(i)))*gsl_sf_bessel_il_scaled(L, x(i));
-      return y;
+    double bessel_il(double r, int L) {
+      // GSL calculates exp(-|x|)k_l(x)
+      return exp(std::abs(r))*gsl_sf_bessel_il_scaled(L, r);
     }
 
-    arma::vec bessel_kl(const arma::vec & x, int L) {
-      arma::vec y(x);
-      for(size_t i=0;i<x.n_elem;i++)
-        // GSL calculates exp(x)k_l(x)
-	y(i)=exp(-x(i))*gsl_sf_bessel_kl_scaled(L, x(i));
+    arma::vec bessel_il(const arma::vec & r, int L) {
+      arma::vec ret(r.n_elem);
+      for(size_t i=0; i<ret.n_elem; i++)
+        ret(i) = bessel_il(r(i), L);
+      return ret;
+    }
 
-      // The definition in GSL is \sqrt(\pi/(2x)), not \sqrt(2/(\pi x))
-      y /= M_PI_2;
+    double bessel_kl(double r, int L) {
+      // GSL calculates exp(-|x|)k_l(x). Also, the definition in GSL
+      // is \sqrt(\pi/(2x)), not \sqrt(2/(\pi x))
+      return exp(std::abs(r))*gsl_sf_bessel_kl_scaled(L, r) / M_PI_2;
+    }
 
-      return y;
+    arma::vec bessel_kl(const arma::vec & r, int L) {
+      arma::vec ret(r.n_elem);
+      for(size_t i=0; i<ret.n_elem; i++)
+        ret(i) = bessel_kl(r(i), L);
+      return ret;
     }
 
     arma::mat product_tei(const arma::mat & ijint, const arma::mat & klint) {

libhelfem/include/FiniteElementBasis.h
@@ -16,6 +16,7 @@
 #ifndef POLYNOMIAL_BASIS_FINITEELEMENTBASIS_H
 #define POLYNOMIAL_BASIS_FINITEELEMENTBASIS_H
 
+#include <functional>
 #include <armadillo>
 #include <memory>
 #include "PolynomialBasis.h"
@@ -124,9 +125,9 @@ namespace helfem {
        * wq:    quadrature weights
        * f(r):  additional weight function, use nullptr for unit weight
        */
-      arma::mat matrix_element(bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, double (*f)(double)) const;
+      arma::mat matrix_element(bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, const std::function<double(double)> & f) const;
       /// Same as above, but only in a single element
-      arma::mat matrix_element(size_t iel, bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, double (*f)(double)) const;
+      arma::mat matrix_element(size_t iel, bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, const std::function<double(double)> & f) const;
 
       /// Print out the basis functions
       void print(const std::string & str="") const;

libhelfem/src/FiniteElementBasis.cpp
@@ -201,7 +201,7 @@ namespace helfem {
       return p->eval_d2f(x,scaling_factor(iel));
     }
 
-    arma::mat FiniteElementBasis::matrix_element(bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, double (*f)(double)) const {
+    arma::mat FiniteElementBasis::matrix_element(bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, const std::function<double(double)> & f) const {
       arma::mat M(get_nbf(),get_nbf(),arma::fill::zeros);
 
       for(size_t iel=0; iel<get_nelem(); iel++) {
@@ -216,13 +216,13 @@ namespace helfem {
       return M;
     }
 
-    arma::mat FiniteElementBasis::matrix_element(size_t iel, bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, double (*f)(double)) const {
+    arma::mat FiniteElementBasis::matrix_element(size_t iel, bool lhder, bool rhder, const arma::vec & xq, const arma::vec & wq, const std::function<double(double)> & f) const {
       // Get coordinate values
       arma::vec r(eval_coord(xq, iel));
       // Calculate total weight per point
       arma::vec wp(wq*scaling_factor(iel));
       // Include the function
-      if(f != nullptr) {
+      if(f) {
           for(size_t i=0; i<wp.n_elem; i++)
             wp(i)*=f(r(i));
       }

libhelfem/src/RadialBasis.cpp
@@ -17,6 +17,7 @@
 #include "RadialPotential.h"
 #include "chebyshev.h"
 #include "quadrature.h"
+#include "utils.h"
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -76,34 +77,19 @@ namespace helfem {
         return fem.get_poly_order();
       }
 
-      arma::mat RadialBasis::radial_integral(const arma::mat &bf, int Rexp,
-                                size_t iel) const {
-        double Rmin(fem.element_begin(iel));
-        double Rmax(fem.element_end(iel));
-        return quadrature::radial_integral(Rmin, Rmax, Rexp, xq, wq, bf);
-      }
-
       arma::mat RadialBasis::radial_integral(int Rexp, size_t iel) const {
-        arma::mat bf(fem.eval_f(xq, iel));
-        return radial_integral(bf, Rexp, iel);
+        std::function<double(double)> rpowL = [Rexp](double r) { return std::pow(r, Rexp); };
+        return fem.matrix_element(iel, false, false, xq, wq, rpowL);
       }
 
       arma::mat RadialBasis::bessel_il_integral(int L, double lambda, size_t iel) const {
-        double Rmin(fem.element_begin(iel));
-        double Rmax(fem.element_end(iel));
-        arma::mat bf(fem.eval_f(xq, iel));
-
-        // Integral by quadrature
-        return quadrature::bessel_il_integral(Rmin, Rmax, L, lambda, xq, wq, bf);
+        std::function<double(double)> besselil = [L, lambda](double r) { return utils::bessel_il(r*lambda, L); };
+        return fem.matrix_element(iel, false, false, xq, wq, besselil);
       }
 
       arma::mat RadialBasis::bessel_kl_integral(int L, double lambda, size_t iel) const {
-        double Rmin(fem.element_begin(iel));
-        double Rmax(fem.element_end(iel));
-        arma::mat bf(fem.eval_f(xq, iel));
-
-        // Integral by quadrature
-        return quadrature::bessel_kl_integral(Rmin, Rmax, L, lambda, xq, wq, bf);
+        std::function<double(double)> besselkl = [L, lambda](double r) { return utils::bessel_kl(r*lambda, L); };
+        return fem.matrix_element(iel, false, false, xq, wq, besselkl);
       }
 
       arma::mat RadialBasis::radial_integral(const RadialBasis &rh, int n, bool lhder,
@@ -207,8 +193,8 @@ namespace helfem {
       }
 
       arma::mat RadialBasis::kinetic(size_t iel) const {
-        arma::mat df(fem.eval_df(xq, iel));
-        return 0.5 * radial_integral(df, 0, iel);
+        std::function<double(double)> dummy;
+        return 0.5*fem.matrix_element(iel, true, true, xq, wq, dummy);
       }
 
       arma::mat RadialBasis::kinetic_l(size_t iel) const {
@@ -219,12 +205,8 @@ namespace helfem {
 
       arma::mat RadialBasis::model_potential(const modelpotential::ModelPotential *model,
                                              size_t iel) const {
-        double Rmin(fem.element_begin(iel));
-        double Rmax(fem.element_end(iel));
-        arma::mat bf(fem.eval_f(xq, iel));
-
-        // Integral by quadrature
-        return quadrature::model_potential_integral(Rmin, Rmax, model, xq, wq, bf);
+        std::function<double(double)> modelpot = [model](double r) { return model->V(r); };
+        return fem.matrix_element(iel, false, false, xq, wq, modelpot);
       }
 
       arma::mat RadialBasis::nuclear_offcenter(size_t iel, double Rhalf, int L) const {

libhelfem/src/quadrature.cpp
@@ -20,170 +20,6 @@
 
 namespace helfem {
   namespace quadrature {
-    arma::mat radial_integral(double rmin, double rmax, int n, const arma::vec & x, const arma::vec & wx, const arma::mat & bf) {
-#ifndef ARMA_NO_DEBUG
-      if(x.n_elem != wx.n_elem) {
-        std::ostringstream oss;
-        oss << "x and wx not compatible: " << x.n_elem << " vs " << wx.n_elem << "!\n";
-        throw std::logic_error(oss.str());
-      }
-      if(x.n_elem != bf.n_rows) {
-        std::ostringstream oss;
-        oss << "x and bf not compatible: " << x.n_elem << " vs " << bf.n_rows << "!\n";
-        throw std::logic_error(oss.str());
-      }
-#endif
-
-      // Midpoint is at
-      double rmid(0.5*(rmax+rmin));
-      // and half-length of interval is
-      double rlen(0.5*(rmax-rmin));
-      // r values are then
-      arma::vec r(rmid*arma::ones<arma::vec>(x.n_elem)+rlen*x);
-
-      // Calculate total weight per point
-      arma::vec wp(wx*rlen);
-      if(n!=0)
-        wp%=arma::pow(r,n);
-
-      // Put in weight
-      arma::mat wbf(bf);
-      for(size_t i=0;i<bf.n_cols;i++)
-	wbf.col(i)%=wp;
-
-      // Matrix elements are then
-      return arma::trans(wbf)*bf;
-    }
-
-    arma::mat derivative_integral(double rmin, double rmax, const arma::vec & x, const arma::vec & wx, const arma::mat & dbf) {
-#ifndef ARMA_NO_DEBUG
-      if(x.n_elem != wx.n_elem) {
-        std::ostringstream oss;
-        oss << "x and wx not compatible: " << x.n_elem << " vs " << wx.n_elem << "!\n";
-        throw std::logic_error(oss.str());
-      }
-      if(x.n_elem != dbf.n_rows) {
-        std::ostringstream oss;
-        oss << "x and dbf not compatible: " << x.n_elem << " vs " << dbf.n_rows << "!\n";
-        throw std::logic_error(oss.str());
-      }
-#endif
-
-      // Half-length of interval is
-      double rlen(0.5*(rmax-rmin));
-
-      // Put in weight
-      arma::mat wdbf(dbf);
-      for(size_t i=0;i<dbf.n_cols;i++)
-	// We get +1 rlen from the jacobian, but -2 from the derivatives
-	wdbf.col(i)%=wx/rlen;
-
-      // Integral is
-      return arma::trans(wdbf)*dbf;
-    }
-
-    arma::mat model_potential_integral(double rmin, double rmax, const modelpotential::ModelPotential * nuc, const arma::vec & x, const arma::vec & wx, const arma::mat & bf) {
-#ifndef ARMA_NO_DEBUG
-      if(x.n_elem != wx.n_elem) {
-        std::ostringstream oss;
-        oss << "x and wx not compatible: " << x.n_elem << " vs " << wx.n_elem << "!\n";
-        throw std::logic_error(oss.str());
-      }
-      if(x.n_elem != bf.n_rows) {
-        std::ostringstream oss;
-        oss << "x and bf not compatible: " << x.n_elem << " vs " << bf.n_rows << "!\n";
-        throw std::logic_error(oss.str());
-      }
-#endif
-
-      // Midpoint is at
-      double rmid(0.5*(rmax+rmin));
-      // and half-length of interval is
-      double rlen(0.5*(rmax-rmin));
-      // r values are then
-      arma::vec r(rmid*arma::ones<arma::vec>(x.n_elem)+rlen*x);
-
-      // Calculate total weight per point
-      arma::vec wp(wx*rlen);
-      wp%=nuc->V(r);
-
-      // Put in weight
-      arma::mat wbf(bf);
-      for(size_t i=0;i<bf.n_cols;i++)
-	wbf.col(i)%=wp;
-
-      // Matrix elements are then
-      return arma::trans(wbf)*bf;
-    }
-
-    arma::mat bessel_il_integral(double rmin, double rmax, int L, double lambda, const arma::vec & x, const arma::vec & wx, const arma::mat & bf) {
-#ifndef ARMA_NO_DEBUG
-      if(x.n_elem != wx.n_elem) {
-        std::ostringstream oss;
-        oss << "x and wx not compatible: " << x.n_elem << " vs " << wx.n_elem << "!\n";
-        throw std::logic_error(oss.str());
-      }
-      if(x.n_elem != bf.n_rows) {
-        std::ostringstream oss;
-        oss << "x and bf not compatible: " << x.n_elem << " vs " << bf.n_rows << "!\n";
-        throw std::logic_error(oss.str());
-      }
-#endif
-
-      // Midpoint is at
-      double rmid(0.5*(rmax+rmin));
-      // and half-length of interval is
-      double rlen(0.5*(rmax-rmin));
-      // r values are then
-      arma::vec r(rmid*arma::ones<arma::vec>(x.n_elem)+rlen*x);
-
-      // Calculate total weight per point
-      arma::vec wp(wx*rlen);
-      wp%=utils::bessel_il(r*lambda,L);
-
-      // Put in weight
-      arma::mat wbf(bf);
-      for(size_t i=0;i<bf.n_cols;i++)
-	wbf.col(i)%=wp;
-
-      // Matrix elements are then
-      return arma::trans(wbf)*bf;
-    }
-
-    arma::mat bessel_kl_integral(double rmin, double rmax, int L, double lambda, const arma::vec & x, const arma::vec & wx, const arma::mat & bf) {
-#ifndef ARMA_NO_DEBUG
-      if(x.n_elem != wx.n_elem) {
-        std::ostringstream oss;
-        oss << "x and wx not compatible: " << x.n_elem << " vs " << wx.n_elem << "!\n";
-        throw std::logic_error(oss.str());
-      }
-      if(x.n_elem != bf.n_rows) {
-        std::ostringstream oss;
-        oss << "x and bf not compatible: " << x.n_elem << " vs " << bf.n_rows << "!\n";
-        throw std::logic_error(oss.str());
-      }
-#endif
-
-      // Midpoint is at
-      double rmid(0.5*(rmax+rmin));
-      // and half-length of interval is
-      double rlen(0.5*(rmax-rmin));
-      // r values are then
-      arma::vec r(rmid*arma::ones<arma::vec>(x.n_elem)+rlen*x);
-
-      // Calculate total weight per point
-      arma::vec wp(wx*rlen);
-      wp%=utils::bessel_kl(r*lambda,L);
-
-      // Put in weight
-      arma::mat wbf(bf);
-      for(size_t i=0;i<bf.n_cols;i++)
-	wbf.col(i)%=wp;
-
-      // Matrix elements are then
-      return arma::trans(wbf)*bf;
-    }
-
     static arma::vec twoe_inner_integral_wrk(double rmin, double rmax, double rmin0, double rmax0, const arma::vec & x, const arma::vec & wx, const std::shared_ptr<const polynomial_basis::PolynomialBasis> & poly, int L) {
       // Midpoint is at
       double rmid(0.5*(rmax+rmin));

libhelfem/src/quadrature.h
@@ -42,44 +42,6 @@ namespace helfem {
     arma::mat derivative_integral(double rmin, double rmax, const arma::vec & x, const arma::vec & wx, const arma::mat & dbf);
 
     /**
-     * Computes a radial integral of the type \f$ \int_0^\infty B_1 (r) B_2(r) Vnuc(r) dr \f$.
-     *
-     * Input
-     *   rmin: start of element boundary
-     *   rmax: end of element boundary
-     *      x: integration nodes
-     *     wx: integration weights
-     *     bf: basis functions evaluated at integration nodes.
-     */
-    arma::mat model_potential_integral(double rmin, double rmax, const modelpotential::ModelPotential * nuc, const arma::vec & x, const arma::vec & wx, const arma::mat & bf);
-
-    /**
-     * Computes a radial integral of the type \f$ \int_0^\infty B_1 (r) B_2(r) i_L(\lambda r) dr \f$.
-     *
-     * Input
-     *   rmin: start of element boundary
-     *   rmax: end of element boundary
-     *      L: Bessel function order
-     *      x: integration nodes
-     *     wx: integration weights
-     *     bf: basis functions evaluated at integration nodes.
-     */
-    arma::mat bessel_il_integral(double rmin, double rmax, int L, double lambda, const arma::vec & x, const arma::vec & wx, const arma::mat & bf);
-
-    /**
-     * Computes a radial integral of the type \f$ \int_0^\infty B_1 (r) B_2(r) k_L(\lambda r) dr \f$.
-     *
-     * Input
-     *   rmin: start of element boundary
-     *   rmax: end of element boundary
-     *      L: Bessel function order
-     *      x: integration nodes
-     *     wx: integration weights
-     *     bf: basis functions evaluated at integration nodes.
-     */
-    arma::mat bessel_kl_integral(double rmin, double rmax, int L, double lambda, const arma::vec & x, const arma::vec & wx, const arma::mat & bf);
-
-    /**
      * Computes the inner in-element two-electron integral:
      * \f$ \phi(r) = \frac 1 r^{L+1} \int_0^r dr' r'^{L} B_k(r') B_l(r') \f$
      */

libhelfem/src/utils.h
@@ -30,7 +30,11 @@ namespace helfem {
     arma::vec arsinh(const arma::vec & x);
 
     /// Modified Bessel function
+    double bessel_il(double x, int L);
+    /// Modified Bessel function
     arma::vec bessel_il(const arma::vec & x, int L);
+    /// Modified Bessel function
+    double bessel_kl(double x, int L);
     /// Modified Bessel function
     arma::vec bessel_kl(const arma::vec & x, int L);
 

commit a17fa561ac2dd6236899682193fc9075ddfc1e95

bessel_kl got broken by commit d62f1c1b; this is the right definition

libhelfem/src/utils.cpp
@@ -57,9 +57,9 @@ namespace helfem {
     }
 
     double bessel_kl(double r, int L) {
-      // GSL calculates exp(-|x|)k_l(x). Also, the definition in GSL
+      // GSL calculates exp(x)k_l(x). Also, the definition in GSL
       // is \sqrt(\pi/(2x)), not \sqrt(2/(\pi x))
-      return exp(std::abs(r))*gsl_sf_bessel_kl_scaled(L, r) / M_PI_2;
+      return exp(-r)*gsl_sf_bessel_kl_scaled(L, r) / M_PI_2;
     }
 
     arma::vec bessel_kl(const arma::vec & r, int L) {