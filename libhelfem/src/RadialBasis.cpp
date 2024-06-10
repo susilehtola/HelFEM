45a53e09be0a3d8ef9c9f5cfe2e27c57e882795c
Hugo Åström <hugo.astrom@helsinki.fi>
2024-06-10 21:02:22 +0300

    Fix buf in exponential confinement

--- libhelfem/src/RadialBasis.cpp
+++ libhelfem/src/RadialBasis.cpp
@@ -383,6 +383,7 @@ namespace helfem {
 	  }
 	  V += std::exp(r_ratio);
 	  V *= factorial(N);
+	  V *= std::pow(r, 2);
 	  return V;
 	};
 	std::function<arma::mat(const arma::vec &, size_t)> radial_bf;
