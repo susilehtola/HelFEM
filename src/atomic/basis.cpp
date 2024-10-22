1fec133c8a6623b2486b8c24b1f2d949077b1b83
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2024-10-22 15:18:43 +0300

    Update src/atomic/basis.cpp

--- src/atomic/basis.cpp
+++ src/atomic/basis.cpp
@@ -10,7 +10,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either vbrsion 2
+ * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  */
 #include "basis.h"
