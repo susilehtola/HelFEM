bc0e6a148cc64231578998584262104ebeffdb78
Susi Lehtola <susi.lehtola@gmail.com>
2025-05-23 15:34:31 +0200

    1/(4*pi*epsilon_0) = 1 in atomic units

--- src/sadatom/basis.cpp
+++ src/sadatom/basis.cpp
@@ -107,7 +107,7 @@ namespace helfem {
 
       helfem::Matrix TwoDBasis::background_potential(double rs, double r_inner, double r_outer) const {
         std::function<double(double, double)> sphere_pot = [&](double r, double R) {
-          const double prefac = std::pow(R/rs,3)/(4.0*M_PI);
+          const double prefac = std::pow(R/rs,3);
           if(r<R) {
             return prefac*(3.0 - std::pow(r/R,2))/(2*R);
           } else {
